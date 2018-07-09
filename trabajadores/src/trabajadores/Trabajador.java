cb91e136c2d8f971926c372c0694a7f896c2430c
wendysoto <38251149+wendysoto@users.noreply.github.com>
2018-07-08 22:12:52 -0500

    Add files via upload

---
+++ trabajadores/src/trabajadores/Trabajador.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajadores;
+
+
+
+/**
+ *
+ * @author Wendy Soto
+ */
+public class Trabajador {
+    private String nombre;
+    private String apellido;
+    private String cargo;
+    
+
+    public Trabajador(String nombre, String apellido, String cargo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cargo = cargo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+   
+
+    @Override
+    public String toString() {
+        return   nombre +   apellido    + cargo ;
+    }
+
+    
+    
+}
